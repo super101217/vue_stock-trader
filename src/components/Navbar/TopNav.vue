9b7d2cf9c969dbe95f78071b988e762f8007fef5
Superstar0107 <am000hw@gmail.com>
2018-04-21 20:10:43 +0530

    Install Vue Awesome, add caret-down icon to nav-bar top

--- src/components/Navbar/TopNav.vue
+++ src/components/Navbar/TopNav.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="top-nav-section">
         <div class="top-nav w-container">
-            <a href="#" class="link link-br">Bloomberg the Company &amp; Its Products</a>
+            <a href="#" class="link link-br">Bloomberg the Company &amp; Its Products
+                <span><icon name="caret-down"></icon></span>
+            </a>
             <a href="#" class="link link-br">Bloomberg Anywhere Remote Login</a>
             <a href="#" class="link">Bloomberg Terminal Demo Request</a>
         </div>
@@ -9,8 +11,13 @@
 </template>
 
 <script scoped>
+    // Font Awesome Icon
+    import 'vue-awesome/icons/caret-down'
+    import Icon from 'vue-awesome/components/Icon'
     export default {
-        
+        components: {
+            icon: Icon,
+        }
     }
 </script>
 
@@ -19,5 +26,13 @@
     @import './../../Assets/normalize.css';
     @import './../../Assets/webflow.css';
     @import './../../Assets/custom.css';
+
+    svg {
+        color: lightgray;
+        height: 14px;
+    }
+    svg.fa-icon {
+        padding: 0.2em 0 0 0;
+    }
 
 </style>
