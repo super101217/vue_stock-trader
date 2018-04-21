b98d7faac38caaa855ad06878654bd2850f3785b
Superstar0107 <am000hw@gmail.com>
2018-04-21 19:56:47 +0530

    Render Top Nav Component

---
+++ src/components/Navbar/TopNav.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="top-nav-section">
+        <div class="top-nav w-container">
+            <a href="#" class="link link-br">Bloomberg the Company &amp; Its Products</a>
+            <a href="#" class="link link-br">Bloomberg Anywhere Remote Login</a>
+            <a href="#" class="link">Bloomberg Terminal Demo Request</a>
+        </div>
+    </div>
+</template>
+
+<script scoped>
+    export default {
+        
+    }
+</script>
+
+
+<style>
+    @import './../../Assets/normalize.css';
+    @import './../../Assets/webflow.css';
+    @import './../../Assets/custom.css';
+
+</style>
