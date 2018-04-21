48ddfc898bb720a1f86ef7c87813b1075ce208c3
Superstar0107 <am000hw@gmail.com>
2018-04-21 19:48:38 +0530

    Project Setup and Component Structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <!-- Our Nav Bar Components-->
+      <top-nav-bar />
+      <main-nav-bar />
+      <!-- Main Content and Buttons -->
+      <content-section-one />
+      <content-section-two />
+      <!-- Footer -->
+      <footer-left />
+      <footer-right />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import our components from component directory
+import TopBar from './components/Navbar/TopNav.vue'
+import MainBar from './components/Navbar/MainNav.vue'
+import SectionOne from './components/MainContent/SectionOne.vue'
+import SectionTwo from './components/MainContent/SectionTwo.vue'
+import FooterLeft from './components/Footer/FooterLeft.vue'
+import FooterRight from './components/Footer/FooterRight.vue'
 
+// Use our components in our App.vue template
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    topNavBar: TopBar,
+    mainNavBar: MainBar,
+    contentSectionOne: SectionOne,
+    contentSectionTwo: SectionTwo,
+    footerLeft: FooterLeft,
+    footerRight: FooterRight
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
