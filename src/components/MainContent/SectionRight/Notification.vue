aae0930f2a3d79f69be2c4282324802b8ba16cac
Superstar0107 <am000hw@gmail.com>
2018-04-23 18:32:30 +0530

    Feat: Add Notification Component

---
+++ src/components/MainContent/SectionRight/Notification.vue
@@ -0,0 +1,43 @@
+<template>
+        <div class="text-block">
+            Before it&#x27;s here, it&#x27;s on the Bloomberg Terminal.
+            <icon name="desktop"></icon>
+            <icon name="desktop"></icon>
+            <a class="link">Learn More</a>
+        </div>
+</template>
+
+<script>
+    // Font Awesome Icon
+    import 'vue-awesome/icons/desktop'
+    import Icon from 'vue-awesome/components/Icon'
+
+    export default {
+        components: {
+            icon: Icon
+        }
+    }
+</script>
+
+<style scoped>
+    @import './../../../Assets/normalize.css';
+    @import './../../../Assets/webflow.css';
+    @import './../../../Assets/custom.css';
+
+    .text-block {
+        margin-top: 0;
+    }
+
+    .link {
+        margin: 0 0 0 5px;
+        cursor: pointer;
+    }
+
+    /* Icon */
+    svg.fa-icon {
+        color: white;
+        padding: 0.5em 0 0 0;
+    }
+
+
+</style>
---
+++ src/components/MainContent/SectionRight/SectionTwo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="div-block-3 w-clearfix">
+        <notification-window />
+    </div>
+</template>
+
+<script>
+    import Notification from './Notification.vue'
+
+    export default {
+        components: {
+            notificationWindow: Notification
+        }
+    }
+</script>
+
+
+<style scoped>
+    @import './../../../Assets/normalize.css';
+    @import './../../../Assets/webflow.css';
+    @import './../../../Assets/custom.css';
+
+    .div-block-3 {
+        margin-top: 2em;
+    }
+
+</style>
+
