9ea6eab01595d854c3c588ba5a9e8b71f9b74c8a
Superstar0107 <am000hw@gmail.com>
2018-04-24 19:17:57 +0530

    Add icon to graph filter component

--- src/components/MainContent/SectionRight/Content/SectionContent/GraphFilter.vue
+++ src/components/MainContent/SectionRight/Content/SectionContent/GraphFilter.vue
@@ -1,37 +1,45 @@
 <template>
     <div class="div-block-12">
         <div class="div-block-7 time-frame-box">
-        <div class="time-box-one b-r w-clearfix">
-            <div class="text-block-6 selected">1D</div>
-        </div>
-        <div class="time-box-one b-r w-clearfix">
-            <div class="text-block-6">1M</div>
-        </div>
-        <div class="time-box-one b-r w-clearfix">
-            <div class="text-block-6">1Y</div>
-        </div>
-        <div class="time-box-one b-r w-clearfix">
-            <div class="text-block-6">5Y</div>
-        </div>
-        <div class="time-box-one time-label-box w-clearfix">
-            <div class="text-block-6 text-label">Time Frame</div>
-        </div>
+            <div class="time-box-one b-r w-clearfix">
+                <div class="text-block-6 selected">1D</div>
+            </div>
+            <div class="time-box-one b-r w-clearfix">
+                <div class="text-block-6">1M</div>
+            </div>
+            <div class="time-box-one b-r w-clearfix">
+                <div class="text-block-6">1Y</div>
+            </div>
+            <div class="time-box-one b-r w-clearfix">
+                <div class="text-block-6">5Y</div>
+            </div>
+            <div class="time-box-one time-label-box w-clearfix">
+                <div class="text-block-6 text-label">Time Frame</div>
+            </div>
         </div>
         <div class="div-block-7 time-frame-box time-frame-box-two input-box w-clearfix">
-        <div class="text-block-7">
-            <input placeholder="Add Comparison" />
-        </div>
-        <div class="div-block-13"></div>
+            <div class="text-block-7">
+                <input placeholder="Add Comparison" />
+            </div>
+            <div class="div-block-13">
+                <icon name="plus"></icon>
+            </div>
         </div>
         <div class="div-block-7 time-frame-box time-frame-box-two time-frame-box-three w-clearfix">
-        <div class="text-block-7 text-drop-down">Indicators</div>
+            <div class="text-block-7 text-drop-down">Indicators</div>
         </div>
     </div>
 </template>
 
 <script>
+    // Font Awesome Icon
+    import 'vue-awesome/icons/plus'
+    import Icon from 'vue-awesome/components/Icon'
+
     export default {
-    
+        components: {
+            icon: Icon
+        }
     }
 </script>
 
@@ -54,6 +62,13 @@
         border: none;
         outline: none;
         width: 8.5em;
+    }
+
+    /* Icon */
+    svg.fa-icon {
+        color: white;
+        margin: 0.5em 0.7em;
+        cursor: pointer;
     }
 
 </style>
--- src/components/MainContent/SectionRight/Content/SectionContent/SectionContent.vue
+++ src/components/MainContent/SectionRight/Content/SectionContent/SectionContent.vue
@@ -4,10 +4,12 @@
         <div>
             <!-- Graph Filter Component -->
             <graph-filter-selection />
+            <!-- Chart -->
             <div class="pattern-box-border chart-box">
                 <div class="div-block-8"></div>
             </div>
             <div class="div-block-5 pattern-div"></div>
+            <!-- Table -->
             <div class="table-div"></div>
         </div>
     </div>
