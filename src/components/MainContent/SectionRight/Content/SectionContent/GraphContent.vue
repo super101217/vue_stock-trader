b3ac26d49ad35566c4a12198c671d3c825f07c7a
Superstar0107 <am000hw@gmail.com>
2018-04-25 17:20:12 +0530

    Refactor: Graph Component - Use chart js line chart to show S&P index

--- src/components/MainContent/SectionRight/Content/SectionContent/GraphContent.vue
+++ src/components/MainContent/SectionRight/Content/SectionContent/GraphContent.vue
@@ -1,38 +1,43 @@
 <template>
     <!-- Our Chart Div -->
-    <div class="div-block-8">
-        <div id="chartdiv" style="width: 100%; height: 350px;"></div>
+    <div class="bg">
+        <canvas id="planet-chart"></canvas>
     </div>
 </template>
 
 <script>
+    import Chart from 'chart.js';
+    import planetChartData from './GraphContentData.js'
+
     export default {
-        created () {
-            AmCharts.makeChart ("chartdiv",
-                {
-                    "type": "serial",
-                    "categoryField": "type",
-                    "chartCursor": {},
-                    "graphs": [
-                        {
-                            "type": "column",
-                            "title": "Pizza types",
-                            "valueField": "sold",
-                            "fillAlphas": 0.8
-                        }
-                    ],
-                    "dataProvider": [
-                        { "type": "Margherita", "sold": 120 },
-                        { "type": "Funghi", "sold": 82 },
-                        { "type": "Capricciosa", "sold": 78 },
-                        { "type": "Quattro Stagioni", "sold": 71 }
-                    ]
-                }
-            );
+        methods: {
+            createChart(chartId, chartData) {
+                const ctx = document.getElementById(chartId);
+                const myChart = new Chart(ctx, {
+                    type: chartData.type,
+                    data: chartData.data,
+                    options: chartData.options,
+                });
+            }
+        },
+        data() {
+            return {
+                planetChartData: planetChartData,
+            }
+        },
+        mounted() {
+            this.createChart('planet-chart', this.planetChartData);
         }
     }
 </script>
 
 <style scoped>
+
+    .planet-chart {
+        padding: 1em;
+    }
+    .bg {
+        background-color: #2b343f;
+    }
 
 </style>
