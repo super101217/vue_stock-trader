df2cd813084004e09c618d4de3a4e8a191349c14
Superstar0107 <am000hw@gmail.com>
2018-04-26 14:47:03 +0530

    Refactor: Add IND index to graph line data

--- src/components/MainContent/SectionRight/Content/SectionContent/Graph/GraphContentData.js
+++ src/components/MainContent/SectionRight/Content/SectionContent/Graph/GraphContentData.js
@@ -1,9 +1,21 @@
 export const planetChartData = {
     type: 'line',
     data: {
-        labels: ['11a', '12p', '1p', '2p', '3p', '4p'],
+        labels: ['11a', '12p', '1p', '2p', '3p', '4p', '5p'],
         datasets: [
-            { // one line graph
+            { // first line graph
+                label: 'INDU:IND',
+                data: [0.74, 0.62, 0.58, 0.43, 0.31, 0.34, 0.51, 0.47],
+                fill: false,
+                backgroundColor: [
+                    '#fb8b1e', // Orange
+                ],
+                borderColor: [
+                    '#fb8b1e',
+                ],
+                borderWidth: 3
+            },
+            { // another line graph
                 label: 'SPX:IND',
                 data: [0.78, 0.64, 0.61, 0.28, 0.37, 0.42, 0.27, 0.38],
                 fill: false,
