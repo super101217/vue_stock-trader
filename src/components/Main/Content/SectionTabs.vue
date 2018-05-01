8b6fee0fae4ffa766d28f06bb00e2481f55a5f84
Superstar0107 <am000hw@gmail.com>
2018-05-01 14:25:20 +0530

    Feat: Add routes to tab links

--- src/components/Main/Content/SectionTabs.vue
+++ src/components/Main/Content/SectionTabs.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="div-block-4 main-tab-div">
-        <h3 class="heading-3 main-tabs selected">Overview</h3>
-        <h3 class="heading-3 main-tabs">Futures</h3>
+        <router-link to="/stocks" class="router-link">
+            <h3 class="heading-3 main-tabs selected">Overview</h3>
+        </router-link>
+        <router-link to="/futures" class="router-link">
+            <h3 class="heading-3 main-tabs">Futures</h3>
+        </router-link>
         <h3 class="heading-3 main-tabs">Americas</h3>
         <h3 class="heading-3 main-tabs">EMEA</h3>
         <h3 class="heading-3 main-tabs">APAC</h3>
@@ -15,6 +19,10 @@
 </script>
 
 <style scoped>
+    .router-link{
+        text-decoration: none;
+        color: black;
+    }
     .heading-3 {
         cursor: pointer;
     }
