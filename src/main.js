95f2095e2f1973138ca72b00b33d592d1087cd28
Superstar0107 <am000hw@gmail.com>
2018-05-01 13:22:00 +0530

    Feat: Install and setup vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import VueRouter from './vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App)
