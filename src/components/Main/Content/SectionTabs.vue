220391a7cb7a20cbae9857f7a022c3ba80d22659
Superstar0107 <am000hw@gmail.com>
2018-05-01 18:19:59 +0530

    Feat: Add active css class to section tabs

--- src/components/Main/Content/SectionTabs.vue
+++ src/components/Main/Content/SectionTabs.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="div-block-4 main-tab-div">
-        <router-link to="/stocks" class="router-link">
-            <h3 class="heading-3 main-tabs selected">Overview</h3>
+        <router-link to="/stocks" class="router-link" activeClass="selected">
+            <h3 class="heading-3 main-tabs">Overview</h3>
         </router-link>
-        <router-link to="/futures" class="router-link">
+        <router-link to="/futures" class="router-link" activeClass="selected">
             <h3 class="heading-3 main-tabs">Futures</h3>
         </router-link>
         <h3 class="heading-3 main-tabs">Americas</h3>
